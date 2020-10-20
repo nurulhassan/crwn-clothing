d9ec625a680354fb0fe65f81a5d0fc81885a560f
nurulhassan <nurulhassan2000@gmail.com>
2020-10-20 16:25:49 +0530

    sign-up page code

--- src/App.js
+++ src/App.js
@@ -14,32 +14,44 @@ class App extends React.Component {
       currentUser: null
     }
   }
-
+  
   unsubscribeFromAuth= null;
-
+  
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const  userRef= await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+        
+      }
+      this.setState({currentUser:userAuth});
     });
   }
-
+  
   componentWillUnmount(){
     this.unsubscribeFromAuth();
   }
-     
+  
   render () {
     return (
       <div> 
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUpPage}/>
-        </Switch>
+      <Header currentUser={this.state.currentUser} />
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop' component={ShopPage}/>
+      <Route path='/signin' component={SignInAndSignUpPage}/>
+      </Switch>
       </div>
-    );
+      );
+    }
   }
- }
-
-
-export default App;
+  
+  
+  export default App;
